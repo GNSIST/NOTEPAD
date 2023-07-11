fcf671624b73d9b3dfbd2d4ce91a7a1d5921f98d
GNSIST <138013020+GNSIST@users.noreply.github.com>
2023-07-11 22:47:10 +0900

    BONOTEPAD Ver 0.2.0
    계속하면 되는거야 강력한 무기는 그냥 하는거야 나중에 이 메세지를 보고 엄청나게 기뻐하는 날이 반드시 올거야 그러니깐 좀더 달리자

---
+++ Project/script.js
@@ -0,0 +1,65 @@
+$(document).ready(function() {
+   $('#button1').click(function() {
+     alert('파일을 불러옵니다')
+   });
+ 
+   $('#button2').click(function() {
+      alert('저장되었습니다');
+   });
+ });
+
+ $(document).ready(function() {
+   $('#button3').click(function() {
+     alert('공유되었습니다')
+   });
+});
+$(document).ready(function(){
+   $('#button4').click(function(){
+      alert('오렸습니다')
+   });
+});
+$(document).ready(function(){
+   $('#button5').click(function(){
+      alert('복사했습니다')
+   })
+})
+$(document).ready(function(){
+   $('#button6').click(function(){
+      alert('붙여넣기했습니다')
+   })
+})
+$(document).ready(function(){
+   $('#button7').click(function(){  
+      alert('뒤로갔습니다')
+   });
+});
+$(document).ready(function(){
+   $('#button8').click(function(){  
+      alert('앞으로갔습니다')
+   });
+});
+$(document).ready(function(){
+   $('#button9').click(function(){
+      alert('삭제했습니다')
+   });
+});
+$(document).ready(function(){
+   $('#button10').click(function(){
+   alert('확대되었습니다')
+   });
+});
+$(document).ready(function(){
+   $('#button11').click(function(){
+      alrert('축소되었습니다')
+   });
+});
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+   
